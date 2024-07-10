a70bed7a4903409eceb079d549036399501ea042
KevinASencio <x.rid3r1@gmail.com>
2024-07-10 11:44:50 -0600

    lkj

--- GUI/FormsGestion/frmVistaClientes.Designer.cs
+++ GUI/FormsGestion/frmVistaClientes.Designer.cs
@@ -29,14 +29,8 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmVistaClientes));
-            System.Windows.Forms.DataGridView dtgvvClientes;
+            System.Windows.Forms.DataGridView dgtvClientes;
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
-            this.txbFiltro = new System.Windows.Forms.ToolStripTextBox();
-            this.toolStripCerrar = new System.Windows.Forms.ToolStripButton();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.lblRegistro = new System.Windows.Forms.ToolStripStatusLabel();
             this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Nombres = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Apellidos = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -44,11 +38,66 @@
             this.DUI = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            dtgvvClientes = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.txbFiltro = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripCerrar = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblRegistro = new System.Windows.Forms.ToolStripStatusLabel();
+            dgtvClientes = new System.Windows.Forms.DataGridView();
             this.toolStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(dtgvvClientes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(dgtvClientes)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "idcliente";
+            this.Id.HeaderText = "idcliente";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            // 
+            // Nombres
+            // 
+            this.Nombres.DataPropertyName = "nombres";
+            this.Nombres.HeaderText = "nombres";
+            this.Nombres.Name = "Nombres";
+            this.Nombres.ReadOnly = true;
+            // 
+            // Apellidos
+            // 
+            this.Apellidos.DataPropertyName = "apellidos";
+            this.Apellidos.HeaderText = "apellidos";
+            this.Apellidos.Name = "Apellidos";
+            this.Apellidos.ReadOnly = true;
+            // 
+            // Direccion
+            // 
+            this.Direccion.DataPropertyName = "dirrecion";
+            this.Direccion.HeaderText = "direccion ";
+            this.Direccion.Name = "Direccion";
+            this.Direccion.ReadOnly = true;
+            // 
+            // DUI
+            // 
+            this.DUI.DataPropertyName = "dui";
+            this.DUI.HeaderText = "dui";
+            this.DUI.Name = "DUI";
+            this.DUI.ReadOnly = true;
+            // 
+            // Estado
+            // 
+            this.Estado.DataPropertyName = "estado";
+            this.Estado.HeaderText = "estado";
+            this.Estado.Name = "Estado";
+            this.Estado.ReadOnly = true;
+            // 
+            // Telefono
+            // 
+            this.Telefono.DataPropertyName = "telefono";
+            this.Telefono.HeaderText = "telefono";
+            this.Telefono.Name = "Telefono";
+            this.Telefono.ReadOnly = true;
             // 
             // toolStrip1
             // 
@@ -101,14 +150,14 @@
             this.lblRegistro.Size = new System.Drawing.Size(133, 17);
             this.lblRegistro.Text = "0 Registros encontrados";
             // 
-            // dtgvvClientes
+            // dgtvClientes
             // 
-            dtgvvClientes.AllowUserToAddRows = false;
-            dtgvvClientes.AllowUserToDeleteRows = false;
-            dtgvvClientes.AllowUserToOrderColumns = true;
-            dtgvvClientes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            dtgvvClientes.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            dtgvvClientes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dgtvClientes.AllowUserToAddRows = false;
+            dgtvClientes.AllowUserToDeleteRows = false;
+            dgtvClientes.AllowUserToOrderColumns = true;
+            dgtvClientes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dgtvClientes.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            dgtvClientes.BorderStyle = System.Windows.Forms.BorderStyle.None;
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -116,9 +165,9 @@
             dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            dtgvvClientes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            dtgvvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dtgvvClientes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            dgtvClientes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dgtvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgtvClientes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Id,
             this.Nombres,
             this.Apellidos,
@@ -126,71 +175,22 @@
             this.DUI,
             this.Estado,
             this.Telefono});
-            dtgvvClientes.Dock = System.Windows.Forms.DockStyle.Fill;
-            dtgvvClientes.Location = new System.Drawing.Point(0, 25);
-            dtgvvClientes.MultiSelect = false;
-            dtgvvClientes.Name = "dtgvvClientes";
-            dtgvvClientes.ReadOnly = true;
-            dtgvvClientes.RowHeadersVisible = false;
-            dtgvvClientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            dtgvvClientes.Size = new System.Drawing.Size(800, 403);
-            dtgvvClientes.TabIndex = 6;
-            // 
-            // Id
-            // 
-            this.Id.DataPropertyName = "idcliente";
-            this.Id.HeaderText = "idcliente";
-            this.Id.Name = "Id";
-            this.Id.ReadOnly = true;
-            // 
-            // Nombres
-            // 
-            this.Nombres.DataPropertyName = "nombres";
-            this.Nombres.HeaderText = "nombres";
-            this.Nombres.Name = "Nombres";
-            this.Nombres.ReadOnly = true;
-            // 
-            // Apellidos
-            // 
-            this.Apellidos.DataPropertyName = "apellidos";
-            this.Apellidos.HeaderText = "apellidos";
-            this.Apellidos.Name = "Apellidos";
-            this.Apellidos.ReadOnly = true;
-            // 
-            // Direccion
-            // 
-            this.Direccion.DataPropertyName = "dirrecion";
-            this.Direccion.HeaderText = "direccion ";
-            this.Direccion.Name = "Direccion";
-            this.Direccion.ReadOnly = true;
-            // 
-            // DUI
-            // 
-            this.DUI.DataPropertyName = "dui";
-            this.DUI.HeaderText = "dui";
-            this.DUI.Name = "DUI";
-            this.DUI.ReadOnly = true;
-            // 
-            // Estado
-            // 
-            this.Estado.DataPropertyName = "estado";
-            this.Estado.HeaderText = "estado";
-            this.Estado.Name = "Estado";
-            this.Estado.ReadOnly = true;
-            // 
-            // Telefono
-            // 
-            this.Telefono.DataPropertyName = "telefono";
-            this.Telefono.HeaderText = "telefono";
-            this.Telefono.Name = "Telefono";
-            this.Telefono.ReadOnly = true;
+            dgtvClientes.Dock = System.Windows.Forms.DockStyle.Fill;
+            dgtvClientes.Location = new System.Drawing.Point(0, 25);
+            dgtvClientes.MultiSelect = false;
+            dgtvClientes.Name = "dgtvClientes";
+            dgtvClientes.ReadOnly = true;
+            dgtvClientes.RowHeadersVisible = false;
+            dgtvClientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            dgtvClientes.Size = new System.Drawing.Size(800, 403);
+            dgtvClientes.TabIndex = 8;
             // 
             // frmVistaClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(dtgvvClientes);
+            this.Controls.Add(dgtvClientes);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.toolStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -201,7 +201,7 @@
             this.toolStrip1.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(dtgvvClientes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(dgtvClientes)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
