64275a85a6f003aa998750a6917d9e2f3f3d7c91
Kevin <x.rid3r1@gmail.com>
2024-08-27 23:52:48 -0600

    inicio creacion de facturas

---
+++ CapaDatos/Entidades/ControlFechasFacturas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos.Entidades
+{
+    public class ControlFechasFacturas
+    {
+        DateTime _FechaVencimiento;
+        DateTime _FechaHasta;//hasta que dia se esta tomando en cuenta para realizar el cobro del consumo
+        string _Mes;
+        DateTime _FechaGeneracion;
+        ControlFechasFacturas() { this.FechaGeneracion = DateTime.Now; }
+        public DateTime FechaVencimiento { get => _FechaVencimiento; set => _FechaVencimiento = value; }
+        public DateTime FechaHasta { get => _FechaHasta; set => _FechaHasta = value; }
+        public string Mes { get => _Mes; set => _Mes = value; }
+        public DateTime FechaGeneracion { get => _FechaGeneracion; set => _FechaGeneracion = value; }
+
+        public Boolean agregar() 
+        {
+            DBOperacion operacion=new DBOperacion();
+            StringBuilder sentencia=new StringBuilder();
+            sentencia.Append("insert into fecha_control_facturas into(");
+            sentencia.Append("fecha_vencimiento, fecha_hasta, mes, fecha_generacion) Values(");
+            sentencia.Append(this.FechaVencimiento.ToString("yyyy,MM,dd") + ", ");
+            sentencia.Append(this.FechaHasta.ToString("yyy,MM,dd") + ", ");
+            sentencia.Append(this.Mes + ", ");
+            sentencia.Append(this.FechaGeneracion.ToString("yyyy,MM,dd") + ");");
+            try { return operacion.Insertar(sentencia.ToString()); }
+            catch(Exception ex) { return false; }
+        }
+    }
+}
--- CapaDatos/Entidades/Facuras.cs
+++ CapaDatos/Entidades/Facuras.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MySqlX.XDevAPI;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -36,15 +38,45 @@ namespace CapaDatos.Entidades
         {
             DBOperacion operacion= new DBOperacion();
             StringBuilder sentencia= new StringBuilder();
-            sentencia.Append("select fact.idfactura, fact.saldo,fact.mora, fact.estado, fact.estado_pago, fact.idservicio, ");
+            sentencia.Append(" select fact.idfactura, fact.saldo,fact.mora, fact.estado, fact.estado_pago, fact.idservicio, ");
             sentencia.Append("fact.cont_pendiente, fact.descuento, fact.idcontrolfecha, fact.comentario, ");
-            sentencia.Append("concat(cli.nombres,\" \", cli.apellidos ) as 'cliente' from facturas as fact, servicios as serv, clientes as cli ");
-            sentencia.Append("where fact.idservicio=serv.idservicio and cli.idcliente=serv.idcliente and ");
-            sentencia.Append("fact.idcontrolfecha=(select idcontrol from fecha_control_factura order by idcontrol desc limit 1);");
+            sentencia.Append("concat(cli.nombres, \" \" , cli.apellidos) as 'cliente' from facturas as fact, servicios as ");
+            sentencia.Append("serv, clientes as cli, serviciosconsumo as con ");
+            sentencia.Append("where fact.idservicio = serv.idservicio and cli.idcliente = serv.idcliente and ");
+            sentencia.Append("serv.idconsumo = con.idserviciosconsumo and  ");
+            sentencia.Append("fact.idcontrolfecha = (select idcontrol from fecha_control_factura order by idcontrol desc limit 1);");
             try 
             {
                 return operacion.Consultar(sentencia.ToString());
             } catch (Exception ex) { return null; }
         }
+        //obtiene las facturas con la informacion necesaria para generar las facturas del mes
+        public DataTable ConsultarGenrar()
+        {
+            DBOperacion operacion = new DBOperacion();
+            StringBuilder sentencia = new StringBuilder();
+            sentencia.Append(" select fact.idfactura, fact.saldo,fact.mora, fact.estado, fact.estado_pago, fact.idservicio, ");
+            sentencia.Append("fact.cont_pendiente, fact.descuento, fact.idcontrolfecha, fact.comentario, cuo.monto as cuota, ");
+            sentencia.Append("concat(cli.nombres, \" \" , cli.apellidos) as 'cliente' from facturas as fact, servicios as ");
+            sentencia.Append("serv, clientes as cli, serviciosconsumo as con, cuotasconsumo as cuo ");
+            sentencia.Append("where fact.idservicio = serv.idservicio and cli.idcliente = serv.idcliente and ");
+            sentencia.Append("serv.idconsumo = con.idserviciosconsumo and cuo.idcuotaconsumo = con.idcuotaconsumo and fact.estado='Valida' and ");
+            sentencia.Append("fact.idcontrolfecha = (select idcontrol from fecha_control_factura order by idcontrol desc limit 1);");
+            try
+            {
+                return operacion.Consultar(sentencia.ToString());
+            }
+            catch (Exception ex) { return null; }
+        }
+        public DataTable GenerarFacturas() 
+        {
+            DataTable Result=new DataTable();
+            DataTable facturas= new DataTable();
+
+            
+            return Result;
+        }
+
+
     }
 }
