bfc4d593a9106356b3724b01324775def49117a5
Kevin <x.rid3r1@gmail.com>
2024-08-29 23:53:31 -0600

    LKJ

--- CapaDatos/Entidades/Facturas.cs
+++ CapaDatos/Entidades/Facturas.cs
@@ -58,11 +58,11 @@ namespace CapaDatos.Entidades
             DBOperacion operacion = new DBOperacion();
             StringBuilder sentencia = new StringBuilder();
             sentencia.Append(" select fact.idfactura, fact.saldo,fact.mora, fact.estado, fact.estado_pago, fact.idservicio, ");
-            sentencia.Append("fact.cont_pendiente, fact.descuento, fact.idcontrolfecha, fact.comentario, cuo.monto as cuota, ");
-            sentencia.Append("concat(cli.nombres, ' ' , cli.apellidos) as 'cliente' from facturas as fact, servicios as ");
+            sentencia.Append("fact.cont_pendiente, fact.descuento, fact.idcontrolfecha, fact.comentario, cuo.monto as cuota ");
+            sentencia.Append(" from facturas as fact, servicios as ");
             sentencia.Append("serv, clientes as cli, serviciosconsumo as con, cuotasconsumo as cuo ");
             sentencia.Append("where fact.idservicio = serv.idservicio and cli.idcliente = serv.idcliente and ");
-            sentencia.Append("serv.idconsumo = con.idserviciosconsumo and cuo.idcuotaconsumo = con.idcuotaconsumo and fact.estado='Valida' and fact.estado_pago='Pendiente' and ");
+            sentencia.Append("serv.idconsumo = con.idserviciosconsumo and cuo.idcuotaconsumo = con.idcuotaconsumo and fact.estado='Valida' and ");
             sentencia.Append("fact.idcontrolfecha = (select idcontrol from fecha_control_facturas order by idcontrol desc limit 1 offset 1);");
             try
             {
@@ -84,8 +84,8 @@ namespace CapaDatos.Entidades
                 sentencia.Clear();
                 if (rw.ItemArray[4].ToString().ToLower() == "pendiente")
                 {
-                    sentencia.Append("insert into facturas (idfactura, saldo, mora, estado, estado_pago, idservicio, cont_pendiente, idcontrolfecha)");
-                    sentencia.Append("values (" + 80664 + cont + ", ");
+                    sentencia.Append("insert into facturas (saldo, mora, estado, estado_pago, idservicio, cont_pendiente, idcontrolfecha)");
+                    sentencia.Append("values (");
                     sentencia.Append((double.Parse(rw.ItemArray[1].ToString()) + double.Parse(rw.ItemArray[10].ToString())) + ", ");
                     sentencia.Append((double.Parse(rw.ItemArray[2].ToString()) + mora) + ", ");
                     sentencia.Append("'Valida', ");
@@ -96,14 +96,35 @@ namespace CapaDatos.Entidades
                     try
                     {
                         if (operacion.Insertar(sentencia.ToString())) { operacion.Actualizar("update facturas set estado='Transferida' where idfactura= " + rw.ItemArray[0] + ";"); }
-                        else Result.Rows.CopyTo(rw.ItemArray, cont);
+                        else { Result.Rows.CopyTo(rw.ItemArray, cont); cont++; }
                     }
                     catch (Exception ex)
                     {
                         Result.Rows.CopyTo(rw.ItemArray, cont);
+                        cont++;
+                    }
+                } 
+                else
+                {
+                    sentencia.Append("insert into facturas (saldo, mora, estado, estado_pago, idservicio, cont_pendiente, idcontrolfecha)");
+                    sentencia.Append("values (");
+                    sentencia.Append(double.Parse(rw.ItemArray[10].ToString()) + ", ");
+                    sentencia.Append("0.00, ");
+                    sentencia.Append("'Valida', ");
+                    sentencia.Append("'Pendiente', ");
+                    sentencia.Append(rw.ItemArray[5].ToString() + ", ");
+                    sentencia.Append("0, ");
+                    sentencia.Append(idcontrol + ")");
+                    try
+                    {
+                        if (!operacion.Insertar(sentencia.ToString())) { Result.Rows.CopyTo(rw.ItemArray, cont); cont++; }
+                    }
+                    catch (Exception ex)
+                    {
+                        Result.Rows.CopyTo(rw.ItemArray, cont);
+                        cont++;
                     }
                 }
-                cont++;
             }
             return Result;
         }
